eea0297b262103e7460635dc5708ab3166009e39
amatherbishop <Wander@Wander.local>
2017-08-15 21:07:44 -0700

    more work on supplies

---
+++ source/site/resources/suppliesManager.js
@@ -0,0 +1,23 @@
+var txtFile = "supplies.txt";
+var file = new File(txtFile);
+var str = "My string of text";
+
+file.open("w"); // open file with write access
+file.writeln("First line of text");
+file.writeln("Second line of text " + str);
+file.write(str);
+file.close();
+
+/// read from file
+
+var txtFile = "c:/test.txt"
+var file = new File(txtFile);
+
+file.open("r"); // open file with read access
+var str = "";
+while (!file.eof) {
+	// read each line of text
+	str += file.readln() + "\n";
+}
+file.close();
+alert(str);
